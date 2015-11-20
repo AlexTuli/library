6388da7226ebbf5204a3150a4279ebdee26069c8
AlexTuli <balexander1995@gmail.com>
2015-11-20 21:29:40 +0600

    Initial commit

---
+++ src/main/java/com/epam/alex/task4/Main.java
@@ -0,0 +1,13 @@
+package com.epam.alex.task4;
+
+/**
+ * Created by AlexTuli on 11/20/15.
+ *
+ * @author Bocharnikov Alexandr
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
