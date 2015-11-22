bc0fc7511e78f4166e4f7caedd65588db019ad03
AlexTuli <balexander1995@gmail.com>
2015-11-22 16:57:13 +0600

    Add readme and dependency

---
+++ readme.md
@@ -0,0 +1,3 @@
+This is 4 task for EPAM systems.
+
+It's a library management system.
---
+++ src/main/java/com/epam/alex/task4/servlet/MainServlet.java
@@ -0,0 +1,26 @@
+package com.epam.alex.task4.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by AlexTuli on 11/22/15.
+ *
+ * @author Bocharnikov Alexandr
+ */
+
+@WebServlet(name = "MainServlet")
+public class MainServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
