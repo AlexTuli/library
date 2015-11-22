259411dc1b65134bb9f1b9f011936e2193179b38
AlexTuli <balexander1995@gmail.com>
2015-11-22 16:06:00 +0600

    Add subscription

---
+++ src/main/java/com/epam/alex/task4/entity/Subscription.java
@@ -0,0 +1,69 @@
+package com.epam.alex.task4.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AlexTuli on 11/22/15.
+ *
+ * @author Bocharnikov Alexandr
+ */
+public class Subscription {
+
+    private String nameOfUser;
+
+    private List<Book> bookList;
+
+    public Subscription() {
+        bookList = new ArrayList<>();
+    }
+
+    public Subscription(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public Book getBook(int index) {
+        return bookList.get(index);
+    }
+
+    public void addBook (Book book) {
+        bookList.add(book);
+    }
+
+    public String getNameOfUser() {
+        return nameOfUser;
+    }
+
+    public void setNameOfUser(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subscription that = (Subscription) o;
+
+        if (!nameOfUser.equals(that.nameOfUser)) return false;
+        return !(bookList != null ? !bookList.equals(that.bookList) : that.bookList != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nameOfUser.hashCode();
+        result = 31 * result + (bookList != null ? bookList.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "This is subscription of user: " + nameOfUser +
+                "\nHe take " + bookList.size() + " books from library";
+    }
+}
