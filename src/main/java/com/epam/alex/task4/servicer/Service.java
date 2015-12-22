430bc5a624c5f65bd2dcb26a169dada5338e498a
AlexTuli <balexander1995@gmail.com>
2015-12-22 22:49:58 +0600

    Little changes

--- src/main/java/com/epam/alex/task4/servicer/Service.java
+++ src/main/java/com/epam/alex/task4/servicer/Service.java
@@ -5,16 +5,24 @@ import org.apache.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Utilites class for clear-up code from same blocks of code
  * Created by AlexTuli on 12/22/15.
  *
  * @author Bocharnikov Alexandr
  */
-public class Service {
+public final class Service {
 
     private static final Logger log = Logger.getLogger(Service.class);
 
+    /**
+     * Get "id" param from request
+     * @return id, or -1 if it's not a number
+     */
     public static int getId(HttpServletRequest request) {
         String stringId = request.getParameter("id");
+        if (stringId == null) {
+            return -1;
+        }
         int id;
         try {
             id = Integer.parseInt(stringId);
